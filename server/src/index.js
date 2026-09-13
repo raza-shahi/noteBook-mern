190ba10010f8441f21a989197f0cbcaa27e65d23
Shahid Raza <raza59261@gmail.com>
2026-09-13 11:17:59 +0530

    prepared for deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -8,21 +9,33 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-
 const app = express();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 connectDB();
 
 // midleware
-app.use(cors())
-// app.use(cors({
-//   origin:"http://localhost:5173/",
-// }))
+if (process.env.NODE_ENV !== "production") {
+  // app.use(cors())
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
+
 app.use(express.json());
-app.use(rateLimiter)
+app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("server is running on port:" + PORT);

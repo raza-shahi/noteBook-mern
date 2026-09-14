d5b9e31d824d5bafe1ba8d7ae4195e060463479b
Shahid Raza <raza59261@gmail.com>
2026-09-14 16:03:44 +0530

    base url updated

--- client/src/libs/axios.js
+++ client/src/libs/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "";
 const api = axios.create({
   baseURL: BASE_URL,
 });
